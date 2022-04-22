e144278639e30e3316877a106c17bfc7adf38f84
John <professional-contact@johnmanuel.info>
2022-04-22 11:28:33 -0700

    File Writing Functions
    added:
    - methods to both file classes to append data to the last row
    - public int for the last row in business file
    
    TODO/Extras:
    - method in boarding pass file to change just one cell

--- src/main/java/com/boardingpass/utils/BusinessLogicFileUtil.java
+++ src/main/java/com/boardingpass/utils/BusinessLogicFileUtil.java
@@ -1,17 +1,23 @@
 package com.boardingpass.utils;
 
 import com.opencsv.CSVReader;
+import com.opencsv.CSVWriter;
 
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.util.List;
 
 public class BusinessLogicFileUtil {
 
     private final File file = new File("resources/BusinessLogic.csv");
 
+    private String[] rowData;
+    private String[] filteredRowData;
+
+    public int lastLineIndex;
+
     public String[] getRow(int chosenRow){
-        String [] rowData = new String[]{"no", "data", "found"};
         try{
             FileReader fileReader = new FileReader(file);
             CSVReader csvReader = new CSVReader(fileReader);
@@ -19,11 +25,59 @@ public class BusinessLogicFileUtil {
             if(csvReader.readNext() != null){
                 List<String[]> allData = csvReader.readAll();
                 rowData = allData.get(chosenRow - 1);
+
+                lastLineIndex = rowData.length;
             }
+            fileReader.close();
+            csvReader.close();
         }
         catch (Exception e){
             e.printStackTrace();
         }
         return rowData;
+    }
+
+    //this method is for internal use. The boardingPass file will be calling this to get the filtered out data
+    //needed to fill out the blank spots in the final document
+    public String[] getFilteredRow(int chosenRow){
+        int z = 0;
+        try{
+            FileReader fileReader = new FileReader(file);
+            CSVReader csvReader = new CSVReader(fileReader);
+
+            if(csvReader.readNext() != null){
+                List<String[]> allData = csvReader.readAll();
+                rowData = allData.get(chosenRow - 1);
+
+                lastLineIndex = rowData.length;
+
+                for(int i = 2; i < rowData.length; i++){
+                    filteredRowData[z] = rowData[i];
+                    z++;
+                }
+            }
+            fileReader.close();
+            csvReader.close();
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        return filteredRowData;
+    }
+
+    public void setRow(String[] inputData){
+        try{
+            FileWriter fileWriter = new FileWriter(file, true);
+            CSVWriter csvWriter = new CSVWriter(fileWriter);
+
+            csvWriter.writeNext(inputData);
+
+            fileWriter.close();
+            csvWriter.close();
+
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
